3519f9091b5cf2bc50ff0e85c1e9740dd69f7663
erik <erikecoologic@gmail.com>
2014-05-31 15:13:40 +1000

    doc

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,7 @@
 class ApplicationController < ActionController::Base
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  # /?q=asdf
   def index
     PgSearch.multisearch(params[:q]) if params[:q].present?
   end
