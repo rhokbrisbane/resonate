be13ec1aa0898a23906f757c232d253b16a15f50
erik <erikecoologic@gmail.com>
2014-05-31 17:08:11 +1000

    org url

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ RhokBrisbane2013::Application.routes.draw do
 
   root :to => "application#index"
 
+  get '/org/:slug', to: 'organisations#org'
 
   devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
